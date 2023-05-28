ea2d88fec5d154cd76bd39c5a6b256a6516febd3
Aditya Raul <raul.aditya912@gmail.com>
2023-05-29 03:12:12 +0530

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import "./App.css";
-
+import styles from "./App.module.css";
+// Another way of using css
 function App() {
   return (
-    <div className="App">
-      <Job salary={10} position="Pocha" company="lastbenchscholar" />
-      <Job salary={20} position="Senior Pocha" company="lastbenchscholar" />
-      <Job salary={30} position="Pocha Head" company="lastbenchscholar" />
+    <div className={styles.App}>
+      <div className={styles.Profile}>Profile</div>
     </div>
   );
 }
-
-function Job(props) {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h4>{props.company}</h4>
-    </div>
-  );
-}
-// normal javascript function
-// function getName() {
-//   return "Aditya";
-// }
-
-// A react component always return jsx(html)
-// first letter must be capital
-// function GetNameComponent() {
-//   return <h1>Aditya</h1>;
-// }
 
 export default App;
