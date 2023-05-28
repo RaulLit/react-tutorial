15f712c973277b74d44628f3a0f00a08d61c4de1
Aditya Raul <raul.aditya912@gmail.com>
2023-05-29 03:07:13 +0530

    Initial Commit & components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job salary={10} position="Pocha" company="lastbenchscholar" />
+      <Job salary={20} position="Senior Pocha" company="lastbenchscholar" />
+      <Job salary={30} position="Pocha Head" company="lastbenchscholar" />
     </div>
   );
 }
+
+function Job(props) {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h4>{props.company}</h4>
+    </div>
+  );
+}
+// normal javascript function
+// function getName() {
+//   return "Aditya";
+// }
+
+// A react component always return jsx(html)
+// first letter must be capital
+// function GetNameComponent() {
+//   return <h1>Aditya</h1>;
+// }
 
 export default App;
