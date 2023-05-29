d15849b168d6d9ca40b8a12c01971febeaf1b1f6
Aditya Raul <raul.aditya912@gmail.com>
2023-05-29 15:54:47 +0530

    react-router complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { useEffect, useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./pages/Navbar";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState();
-
-  const fetchAge = () => {
-    fetch(`https://api.agify.io/?name=${name}`)
-      .then((res) => res.json()) // json() function converts json to javascript object
-      .then((data) => {
-        setAge(data); // always check the structure of the object before storing its values
-      });
-  };
-
   return (
     <div className="App">
-      <input
-        type="text"
-        placeholder="Ex. Aditya.."
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button onClick={fetchAge}>Predict Age</button>
-      <h1>Name : {age?.name}</h1>
-      <h1>Age : {age?.age}</h1>
-      {/* age?.name says that if age object is not null then access name */}
+      <Router>
+        {/* Router specifies where we want to use the react router. 
+          All of router components should be wrapped inside this component */}
+        <Navbar />
+        {/* Navbar should be inside Router(or BrowserRouter) since we are using Link component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
