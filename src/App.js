1fabed684b2a1004bc9107b647651f712efbbb41
Aditya Raul <raul.aditya912@gmail.com>
2023-05-29 14:40:24 +0530

    useEffect complete

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
-import { useState } from "react";
-import { Task } from "./Task";
 import "./App.css";
+import { useState } from "react";
+import { Text } from "./Text";
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
+  const [showText, setShowText] = useState(false);
 
-  const handleChange = (e) => {
-    setNewTask(e.target.value);
-  };
   const handleClick = () => {
-    // const newList = [...todoList, newTask];
-    // setTodoList(newList);
-    // or simply => setTodoList([...todoList, newTask]);
-    const task = {
-      id: todoList.length == 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      complete: false,
-    };
-    setTodoList([...todoList, task]);
-  };
-  const removeTask = (task) => {
-    // setTodoList(todoList.filter((todo) => todo !== task));
-    setTodoList(todoList.filter((todo) => todo.id !== task));
-  };
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((todo) => {
-        if (todo.id == id) {
-          return { ...todo, complete: true };
-        } else {
-          return todo;
-        }
-      })
-    );
+    setShowText(!showText);
   };
 
   return (
     <div className="App">
-      <div className="addTask">
-        <input type="text" onChange={handleChange} />
-        <button onClick={handleClick}>Add Task</button>
-      </div>
-      <div className="list">
-        {todoList.map((val) => {
-          return (
-            <Task
-              task={val}
-              removeTask={removeTask}
-              completeTask={completeTask}
-            />
-          );
-        })}
-      </div>
+      <button onClick={handleClick}>Show/hide Text</button>
+      {showText && <Text />}
     </div>
   );
 }
