3c599a40dd2451969d2eb629178b0da5459a1a50
Aditya Raul <raul.aditya912@gmail.com>
2023-05-31 10:27:14 +0530

    react hook form complete

--- src/App.js
+++ src/App.js
@@ -1,33 +1,10 @@
+import { Form } from "./components/Form";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Navbar } from "./pages/Navbar";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  // passing the object inside QueryClient is optional
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<h1>Page not found</h1>} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
